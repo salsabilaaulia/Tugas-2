23afc79508355ed12fde15abc36838b342f08006
Salsabila Aulia <salsabilaaulia@Salsabilas-MacBook-Pro.local>
2023-09-20 11:05:44 +0700

    mengubah nama models

--- main/models.py
+++ main/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Product(models.Model):
+class Item(models.Model):
     nama = models.CharField(max_length=255)
     kelas = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
--- main/views.py
+++ main/views.py
@@ -2,18 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from main.forms import ProductForm
 from django.urls import reverse
-from main.models import Product
+from main.models import Item
 from django.http import HttpResponse
 from django.core import serializers
 
 # Create your views here.
 def show_main(request):
-    products = Product.objects.all()
+    items = Item.objects.all()
 
     context = {
         'nama' : 'Salsabila Aulia',
         'kelas' : 'PBP C',
-        'products': products
+        'products': items
     }
 
     return render(request, "main.html", context)
@@ -29,17 +29,17 @@ def create_product(request):
     return render(request, "create_product.html", context)
 
 def show_xml(request):
-    data = Product.objects.all()
+    data = Item.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Product.objects.all()
+    data = Item.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
-    data = Product.objects.filter(pk=id)
+    data = Item.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json_by_id(request, id):
-    data = Product.objects.filter(pk=id)
+    data = Item.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
